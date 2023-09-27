6c3a08452aafcf23d62f119a56749fd9fbc09ed7
Zurtar <ethan.cook3552@gmail.com>
2023-09-26 21:04:01 -0400

    Hacky way of getting FCE elective req

--- lu_parser/main.py
+++ lu_parser/main.py
@@ -44,13 +44,20 @@ def parseProgramRequirementsJSON(programFilePath):
     raw_file_json = json.load(raw_file)
 
     program = raw_file_json.get("Program")
-    parsed_requirement_list = [c["Id"]
-                               for R in program.get("Requirements")
-                               for S in R.get("Subrequirements")
-                               for G in S.get("Groups")
-                               for c in (G.get("Courses")
-                                         if len(G.get("Courses")) != 0 else G.get("FromCourses"))
-                               ]
+    parsed_requirement_list = []
+    elective_list = []
+
+    for R in program.get("Requirements"):
+        for S in R.get("Subrequirements"):
+            for G in S.get("Groups"):
+                if len(G.get("Courses")) != 0:
+                    for c in G.get("Courses"):
+                        parsed_requirement_list.append(c.get("Id"))
+                elif len(G.get("FromCourses")) != 0:
+                    for c in G.get("FromCourses"):
+                        parsed_requirement_list.append(c.get("Id"))
+                elif 'ELECTIVE' in G.get("RequirementCode"):
+                    elective_list.append(G.get("RequirementCode").split(".", 1)[1])
 
     parsed_json.update(
         {
@@ -59,7 +66,8 @@ def parseProgramRequirementsJSON(programFilePath):
             "Description": program.get("Description"),
             "Minors": program.get("Minors"),
             "Specializations": program.get("Specializations"),
-            "Requirements": parsed_requirement_list
+            "Requirements": parsed_requirement_list,
+            "Elective": elective_list
         }
     )
 
@@ -69,24 +77,22 @@ def parseProgramRequirementsJSON(programFilePath):
     parsed_file.close()
     raw_file.close()
 
+    # parse their data set by just taking the values we want. It's easier than deleting the rest. maybe we can just load
+    # in specific keys with JSON though
+    def parseCourse(course):
+        parsed_course = dict()
 
-# parse their data set by just taking the values we want. It's easier than deleting the rest. maybe we can just load
-# in specific keys with JSON though
-def parseCourse(course):
-    parsed_course = dict()
-
-    parsed_course.update({
-        "Id": course.get("Id"),
-        "SubjectCode": course.get("SubjectCode"),
-        "CourseCode": course.get("CourseTitleDisplay"),
-        "Title": course.get("Title"),
-        "Description": course.get("Description"),
-        "CourseRequisites": course.get("CourseRequisites"),
-        "Requisites": course.get("Requisites")
-    })
-    return parsed_course
+        parsed_course.update({
+            "Id": course.get("Id"),
+            "SubjectCode": course.get("SubjectCode"),
+            "CourseCode": course.get("CourseTitleDisplay"),
+            "Title": course.get("Title"),
+            "Description": course.get("Description"),
+            "CourseRequisites": course.get("CourseRequisites"),
+            "Requisites": course.get("Requisites")
+        })
+        return parsed_course
 
 
 if __name__ == '__main__':
     parseProgramRequirementsJSON("bio3year.json")
-    parseCourseListJSON()
